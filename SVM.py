31c02a48bd2de48b68b442bd7091e3db3c8e1c0f
Raasika Mageshwaran <121993730+Raasika2510@users.noreply.github.com>
2023-05-17 10:06:15 +0530

    Add files via upload

---
+++ SVM.py
@@ -0,0 +1,28 @@
+# importing libraries
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+from sklearn import metrics
+
+# loading breast_cancer_data from sklearn.dataset
+cancer = datasets.load_breast_cancer()
+x = cancer.data
+y = cancer.target
+# print(x)
+# print(y)
+
+# Data splitting
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
+# print(x_train, y_train)
+classes = ["malignant", "benign"]
+
+# Creating the model
+clas = svm.SVC(kernel="linear")
+clas.fit(x_train, y_train)
+prediction = clas.predict(x_test)
+accuracy = metrics.accuracy_score(y_test, prediction)
+print("Accuracy =", accuracy)
+
+# Displaying the predictions
+for i in prediction:
+    print(classes[i], y_test[i])
